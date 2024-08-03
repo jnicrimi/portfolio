c6c72b48809136dd93a7e498dcd281d426bb59ad
Junichiro Imai <jnicrimi@gmail.com>
2024-08-03 12:45:52 +0900

    style: nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,22 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   typescript: {
     tsConfig: {
       extends: "@tsconfig/strictest/tsconfig.json",
     },
   },
+
   css: ["~/assets/css/main.css", "remixicon/fonts/remixicon.css"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   runtimeConfig: {
     public: {
       contentfulSpaceId: process.env.CTF_SPACE_ID,
       contentfulAccessToken: process.env.CTF_CDA_ACCESS_TOKEN,
     },
   },
+
+  compatibilityDate: "2024-08-03",
 })
